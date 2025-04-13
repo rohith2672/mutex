511899d45b16784f8506154f26503a51c6e84bd3
Rohith <74852170+rohith2672@users.noreply.github.com>
2025-04-13 13:50:13 -0400

    Update distributed_mutex.h

--- distributed_mutex.h
+++ distributed_mutex.h
@@ -1,8 +1,28 @@
 #ifndef DISTRIBUTED_MUTEX_H
 #define DISTRIBUTED_MUTEX_H
 
-int distributed_mutex_init(int my_id, int num_procs, char **hostnames, int *ports, int *shared_balance);
-void distributed_mutex_lock();
-void distributed_mutex_unlock();
+/*
+ * Distributed Mutex Module Interface
+ *
+ * Provides a distributed mutual exclusion mechanism using structured messages
+ * (defined in msg_packet.h) and UDP sockets.
+ *
+ * Parameters:
+ *   local_id       - The local process's ID.
+ *   total_procs    - Total number of processes.
+ *   host_list      - Array of hostnames for each process.
+ *   port_list      - Array of UDP port numbers corresponding to each process.
+ *   acct_ptr       - Pointer to the shared account balance variable.
+ *
+ * The API includes:
+ *   dm_init()          - Initializes the module and spawns the listener thread.
+ *   dm_lock()          - Enters the critical section by obtaining the distributed lock.
+ *   dm_unlock()        - Exits the critical section.
+ *   dm_update_balance() - Broadcasts an updated account balance.
+ */
+int dm_init(int local_id, int total_procs, char **host_list, int *port_list, int *acct_ptr);
+void dm_lock();
+void dm_unlock();
+void dm_update_balance();
 
-#endif
+#endif /* DISTRIBUTED_MUTEX_H */
